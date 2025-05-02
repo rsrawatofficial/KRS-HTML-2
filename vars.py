a1aec9488196ab58aebaec8aeaac9a0f094ba5b8
rsrawatofficial <rsrasasingh@gmail.com>
2025-05-02 20:10:40 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,6 @@
+# Don't Remove Credit Telegram - @mrkrsrawat
+from os import environ
+
+API_ID = int(environ.get("API_ID", "27900743"))
+API_HASH = environ.get("API_HASH", "ebb06ea8d41420e60b29140dcee902fc")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
